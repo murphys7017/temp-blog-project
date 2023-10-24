c6e5edb8df18a5caf52f262466b8a9f22dba35d0
Murphy <54226881+murphys7017@users.noreply.github.com>
2023-10-25 00:26:11 +0800

    基本实现了导航栏，下一步组件化

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,10 +4,23 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <navbar class="button-navbar">
-    <img alt="user avatat" class="botton-avatar" src="https://blog.murphyspolaris.icu/images/avatar.png">
-    
+  <navbar>
+    <img alt="user avatat" class="navbar-avatar" src="https://blog.murphyspolaris.icu/images/avatar.png">
+    <div class="navbar-info">
+      some info
+    </div>
+    <div class="card">
+      <!-- <img class="card-img" src="https://blog.murphyspolaris.icu/images/avatar.png"> -->
+      <div class="card-content">
+        <p>You’ve successfully created a project with Vite + Vue 3. What's next?</p>
+      </div>
+
+    </div>
   </navbar>
+
+
+
+
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
@@ -27,25 +40,81 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-.botton-avatar{
-  height: 80px;
-  width: 80px;
-  border-radius: 50%;
+.card-content {
+  display: flex;
+  align-items: center;
+  height: 40px;
 }
-.button-navbar {
-  width: 90%;
-  height: 80px;
+.card-img {
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  padding: 5px;
+}
+.navbar-info {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 40px;
+  margin: 10px;
+  margin-left: 15px;
+  transform: translateX(0);
+  transition: transform 0.8s;
+}
+.card {
+  display: flex;
+  position: fixed;
+  max-width: 40vw;
+  height: 50px;
+  padding: 10px;
+  flex-direction: row;
+  right: 10px;
+  transition: 
+    margin-top 1.2s,
+    right 1s;
+}
+
+
+.navbar-avatar{
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+  transform: translateX(0);
+  transition: transform 0.8s;
+}
+navbar {
+  display: flex;
+  flex-direction: row;
+  width: 50vw;
+  height: 60px;
   position: fixed;
   bottom: 20px;
   z-index: 10;
-  left: 5%;
-  backdrop-filter: blur(3px);
+  left: 25vw;
+  backdrop-filter: blur(4px);
   float: left;
-  border-radius: 50px 35px 15px 50px
+  border-radius: 50px 35px 15px 50px;
+  transition: 
+    border-radius 1.2s,
+    background-color 2s;
+  
 }
-.button-navbar:hover {
-  background-color: rgba(144, 233, 218, 0.911);
+navbar:hover {
+  background-color: rgba(238, 242, 245, 0.911);
   backdrop-filter: blur(3px);
+  border-radius: 35px 50px 50px 15px;
+}
+navbar:hover .navbar-avatar{
+  transform: translateX(calc(50vw - 60px));
+  transition: transform 0.8s;
+}
+navbar:hover .card{
+  margin-top: -50px;
+  right: 5vw;
+}
+navbar:hover .navbar-info{
+  transform: translateX(-60px);
+  transition: transform 0.8s;
 }
 
 header {
